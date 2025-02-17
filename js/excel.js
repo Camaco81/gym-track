da45a812fd0b8e7bb6ba8c0c3d01c4372f5895b1
camaco81 <camaco81@gmail.com>
2025-02-17 06:55:20 -0400

    Funcion de generar excel con js agregada

---
+++ js/excel.js
@@ -0,0 +1,24 @@
+function generarExcel() {
+  const table = document.getElementById('miTabla');
+  const data = [];
+
+  for (let i = 0; i < table.rows.length; i++) {
+    const row = table.rows[i];
+    const rowData = [];
+    for (let j = 0; j < row.cells.length; j++) {
+      rowData.push(row.cells[j].innerText);
+    }
+    data.push(rowData);
+  }
+
+  const workbook = XLSX.utils.book_new();
+  const worksheet = XLSX.utils.aoa_to_sheet(data);
+  XLSX.utils.book_append_sheet(workbook, worksheet, 'Hoja 1');
+
+  const excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+  const blob = new Blob([excelBuffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+  const link = document.createElement('a');
+  link.href = window.URL.createObjectURL(blob);
+  link.download = 'Clientes.xlsx';
+  link.click();
+}
